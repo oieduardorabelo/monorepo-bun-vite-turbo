a9ebe63753ca89e6e05a6f8cb092c4dc2255e450
Eduardo Rabelo <oieduardorabelo@gmail.com>
2025-04-11 16:17:50 +1200

    chore: init

--- monorepo/apps/ui/src/config/env.ts
+++ monorepo/apps/ui/src/config/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const { VITE_API_URL } = import.meta.env;
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().default("/api"),
+  VITE_API_URL: z.string(),
 });
 
 export const env = envSchema.parse({
